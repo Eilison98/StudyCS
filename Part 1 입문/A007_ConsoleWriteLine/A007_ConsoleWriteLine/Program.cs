91b4deb4453e799fc8a61c9a6637c7ea06b00104
Eilison98 <in05013300@gmail.com>
2022-05-30 18:15:48 +0900

    Part 1 25장 까지

---
+++ Part 1 입문/A007_ConsoleWriteLine/A007_ConsoleWriteLine/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace A007_ConsoleWriteLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool b = true;          //  bool
+            char c = 'A';           //  문자형
+            decimal d = 1.234m;     //  m은 decimal 형의 접미사
+            double e = 1.23456789;  //  실수형
+            float f = 1.23456789f;  //  f는 float 형의 접미사
+            int i = 1234;           //  int 형
+            string s = "Hello";     //  문자열
+            
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.WriteLine(f);
+            Console.WriteLine(i);
+            Console.WriteLine(s);
+        }
+    }
+}
