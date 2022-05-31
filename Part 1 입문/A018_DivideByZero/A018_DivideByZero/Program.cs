df3cd72f7fba491892b71b0942350d26671de1ff
Eilison98 <in05013300@gmail.com>
2022-05-31 13:36:24 +0900

    Part 1 25장 까지

---
+++ Part 1 입문/A018_DivideByZero/A018_DivideByZero/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace A018_DivideByZero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10, y = 0;
+
+            /* 에러 발생
+            Console.WriteLine(10.0 / y);
+            Console.WriteLine(x / y);
+            */
+
+            try
+            {
+                Console.WriteLine(x / y);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
