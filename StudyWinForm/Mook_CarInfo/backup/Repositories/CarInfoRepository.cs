63eccb0f41dd1a32b1e9bd2da5e2108d8b3ba263
Eilison98 <in05013300@gmail.com>
2022-07-05 14:12:13 +0900

    C# 파일합치기 커밋

---
+++ StudyWinForm/Mook_CarInfo/backup/Repositories/CarInfoRepository.cs
@@ -0,0 +1,78 @@
+﻿using Libs;
+using Model;
+using System;
+using System.Collections;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Repositories
+{
+    public class CarInfoRepository : ClassForDBInstance, ICarInfoRepository
+    {
+        private SqlConnection Conn;
+
+        public CarInfoRepository()
+        {
+            Conn = (SqlConnection)configurationMgr.Connection;
+        }
+
+        int ICarInfoRepository.Add(CarInfoModel model, IDbConnection connection)
+        {
+            throw new NotImplementedException();
+        }
+
+        int ICarInfoRepository.Delete(CarInfoModel model, IDbConnection connection)
+        {
+            throw new NotImplementedException();
+        }
+
+        ArrayList ICarInfoRepository.GetAllCarInfo(IDbConnection connection)
+        {
+            try
+            {
+                Conn.Open();
+
+                var Comm = new SqlCommand("Select * From TB_CAR_INFO", Conn);
+                var myRead = Comm.ExecuteReader(CommandBehavior.CloseConnection);
+
+                ArrayList list = new ArrayList();
+
+                while(myRead.Read())
+                {
+                    list.Add(GetCarInfoModel(myRead).ToStringList());
+                }
+                myRead.Close();
+
+                return list;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private CarInfoModel GetCarInfoModel(SqlDataReader myRead)
+        {
+            CarInfoModel model = new CarInfoModel();
+
+            model.id = myRead["id"].ToString();
+            model.carName = myRead["carName"].ToString();
+            model.carYear = myRead["carYear"].ToString();
+            model.carPrice = myRead["carPrice"].ToString();
+            model.carDoor = myRead["carDoor"].ToString();
+
+            return model;
+        }
+
+
+        ArrayList ICarInfoRepository.GetCarInfoByCondition(CarInfoModel model, IDbConnection connection)
+        {
+            throw new NotImplementedException();
+        }
+
+        int ICarInfoRepository.Update(CarInfoModel model, IDbConnection connection)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ StudyWinForm/README.md
@@ -0,0 +1,40 @@
+# WinForm
+
+## Mook_CarInfo
+
+</br>
+
+- 구현기능
+  - MSSQL DB연결을 통한 차관리 시스템
+  - 저장, 수정, 조건검색, 전체검색 
+  - 삭제(마우스 오른쪽 클릭시 활성화)
+
+</br>
+
+<p align = "center">
+<img src ="https://user-images.githubusercontent.com/93025344/173776363-0336a0fc-d6b8-4db0-8ee1-24160a7fe0f0.png" width="350" height="200"> 
+<img src ="https://user-images.githubusercontent.com/93025344/173776378-eec563a0-1c25-4981-b007-d3d4e8540ebd.png" width="350" height="200">
+<img src ="https://user-images.githubusercontent.com/93025344/173776604-0aee035a-95fd-4703-9d60-f505c0265421.png" width="350" height="200">
+<img src ="https://user-images.githubusercontent.com/93025344/173776615-ade59816-c594-4fd7-b117-9203d140339a.png" width="350" height="200">
+</p>
+
+
+## Messanger
+
+</br>
+
+- 구현기능
+  - 서버와 클라이언트 구현
+  - 쓰레드
+  - 1:1 통신
+
+</br>
+
+<p align = "center">
+<img src ="https://user-images.githubusercontent.com/93025344/173778454-3b3f2100-a4d9-4a91-9118-fd679791b830.png" width="390" height="250"> 
+<img src ="https://user-images.githubusercontent.com/93025344/173778465-8eebc8f3-ed8a-43dc-b23b-bf082167d44d.png" width="390" height="250">
+<img src ="https://user-images.githubusercontent.com/93025344/173778485-2e47e1f7-0b13-4bda-a3b7-6f384e63ee45.png" width="390" height="250">
+<img src ="https://user-images.githubusercontent.com/93025344/173778503-70c4b8d2-a7ee-46be-bc21-b9a577f73b4b.png" width="390" height="250">
+</p>
+
+</br>
