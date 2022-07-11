7f3cd73e32c8b8b7b62f5aaa8327eb2c0e296dd2
Eilison98 <in05013300@gmail.com>
2022-07-11 14:52:42 +0900

    Day01

---
+++ C# 공부 Day01 ~ Day10/Day01/Loap.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Loop
+{
+    class forLoop
+    {
+        //  1 ~ 100까지의 합을 구하기
+        static void Main(string[] args)
+        {
+            //  전체 값 초기화
+            int sum = 0;
+
+            //  for문을 통하여 1 ~ 100까지 반복하여 sum에 더해준다.
+            for (int i = 1; i <= 100; i++)
+            {
+                sum = sum + i;
+            }
+
+            //  전체 합의 값을 대입
+            Console.WriteLine("Sum : {0}", sum);
+        }
+    }
+}
---
+++ C# 공부 Day01 ~ Day10/Day01/Multiplication_Table.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Multiplication_Table
+{
+    //  구구단 2 ~ 9단까지
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  * 순서가 중요 *
+            //  for문을 이용하여 2 ~ 9까지 반복
+            for (int i = 2; i < 10; i++)
+            {
+                //  for문을 이용하여 1 ~ 9까지 반복
+                for (int j = 1; j < 10; j++)
+                {
+                    //  {0}자리에 i, {1}자리에 j, {2}자리에 i * j 대입
+                    Console.WriteLine("{0} X {1} = {2}", i, j, i * j);
+                }
+            }
+        }
+    }
+}
