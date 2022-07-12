53a11afa035a36962aafb53abb39db281d157f29
Eilison98 <in05013300@gmail.com>
2022-07-12 11:43:48 +0900

    Day02

---
+++ C# 공부 Day01 ~ Day10/Day02/Menu_Selection.cs
@@ -0,0 +1,62 @@
+﻿/*
+치즈버거, 햄버거, 치킨버거, 베지버거 등 4개의 메뉴를 보여주고 사용자로부터 메뉴를 선택하게 하고, 사용자가 특정 메뉴를 입력하면,
+해당 버거의 가격을 출력하는 프로그램을 작성해 보자. 이 콘솔 프로그램은 무한히 반복되게 작성하는데, 입력시 Q를 치면 루프를 빠져나오게 한다.
+ */
+
+using System;
+
+namespace Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (;;)
+            {
+                Console.WriteLine();
+                Console.WriteLine("******** Menu ********");
+                Console.WriteLine();
+                Console.WriteLine("1. Cheese burger");
+                Console.WriteLine("2. Ham burger");
+                Console.WriteLine("3. Chicken burger");
+                Console.WriteLine("4. Veggie burger");
+                Console.WriteLine();
+
+                Console.Write("Your Choice (Q to Quit) ==> ");
+                string answer = Console.ReadLine();
+
+                Console.WriteLine();
+
+                if (answer == "Q" || answer == "q")
+                {
+                    break;
+                }
+
+                else if (answer == "1")
+                {
+                    Console.WriteLine("Cheese burger : $7.99");
+                }
+
+                else if (answer == "2")
+                {
+                    Console.WriteLine("Ham burger : $6.99");
+                }
+
+                else if (answer == "3")
+                {
+                    Console.WriteLine("Chicken burger : $8.99");
+                }
+
+                else if (answer == "4")
+                {
+                    Console.WriteLine("Veggie burger : $6.00");
+                }
+
+                else
+                {
+                    Console.WriteLine("Error : Invalid Input");
+                }
+            }
+        }
+    }
+}
---
+++ C# 공부 Day01 ~ Day10/Day02/Score_Inquiry.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (; ; )
+            {
+                Console.Write("Enter Score: ");
+                string answer = Console.ReadLine();
+
+                if (answer == "")
+                {
+                    break;
+                }
+
+                int score = int.Parse(answer);
+
+                if (score >= 90)
+                {
+                    Console.WriteLine("Grade A");
+                }
+                else if (score >= 80)
+                {
+                    Console.WriteLine("Grade B");
+                }
+                else if (score >= 70)
+                {
+                    Console.WriteLine("Grade C");
+                }
+                else if (score >= 60)
+                {
+                    Console.WriteLine("Grade D");
+                }
+                else
+                {
+                    Console.WriteLine("Grade F");
+                }
+            }
+        }
+    }
+}
