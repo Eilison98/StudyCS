5527861151eb87c3f0d4024326b0eb945e3011c4
Eilison98 <in05013300@gmail.com>
2022-07-13 12:48:52 +0900

    Day03

---
+++ C# 공부 Day01 ~ Day10/Day03/Arr.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int score1 = 70;
+            int score2 = 80;
+            int score3 = 88;
+            int score4 = 90;
+            int score5 = 75;
+            int score6 = 72;
+            int score7 = 82;
+            int score8 = 85;
+            int score9 = 95;
+            int score10 = 72;
+
+            int sum = score1 + score2 + score3 + score4 + score5 +
+                      score6 + score7 + score8 + score9 + score10;
+
+            Console.WriteLine(sum);
+        }
+    }
+}
+
+
+//  간단하게 사용한다면  //
+/*
+   using System;
+
+  namespace Day3
+  {
+      class Program
+      {
+          static void Main(string[] args)
+          {
+              int[] score = new int[10] { 70, 80, 88, 90, 75, 72, 82, 85, 95, 72 };
+
+              int sum = 0;
+              for (int i = 0; i < score.Length; i++)
+              {
+                  sum += score[i];
+              }
+
+              Console.WriteLine(sum);
+          }
+      }
+  }
+ */
---
+++ C# 공부 Day01 ~ Day10/Day03/Method.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 100;
+            int b = 200;
+
+            int result = Add(a, b);
+            Console.WriteLine(result);
+
+            result = Substract(a, b);
+            Console.WriteLine(result);
+        }
+
+        //  두 정수를 더하는 메서드
+        static int Add(int a, int b)
+        {
+            int c = a + b;
+            return c;
+        }
+
+        //  두 정수를 빼는 메서드
+        static int Substract(int a, int b)
+        {
+            int c = a - b;
+            return c;
+        }
+    }
+}
